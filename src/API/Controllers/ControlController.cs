85b9a60bdd1bf26744ebb6c8cab1150737914dd6
Benjamin Diele <git@diele.be>
2022-03-12 12:34:17 +0100

    Add project files.

---
+++ src/API/Controllers/ControlController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ControlController : ControllerBase
+    {
+        [HttpPost("UpdatePosition")]
+        public Task<IActionResult> UpdatePosition([FromQuery] char direction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
